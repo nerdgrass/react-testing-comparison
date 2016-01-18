126a6defd425440ed8039af105591378ddd2e013
Alex B <admin@admins-MacBook-Pro.local>
2016-01-17 20:55:08 -0700

    scaffold out TodoApp component further

--- src/components/todo-app.js
+++ src/components/todo-app.js
@@ -1,6 +1,29 @@
 import React from 'react';
 
+// Retrieve the current TODO data from the TodoStore
+function getTodoState() {
+  return {
+    allTodos: TodoStore.getAll(),
+    areAllComplete: TodoStore.areAllComplete()
+  };
+}
+
+// Component
 export default class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: getTodoState()};
+  }
+  componentDidMount() {
+    //TodoStore.addChangeListener(this._onChange);
+  }
+  componentWillUnmount() {
+    //TodoStore.removeChangeListener(this._onChange);
+  }
+  // Event handler for 'change' events coming from the TodoStore
+  onChange = () => {
+    this.setState(getTodoState());
+  }
   render() {
     return (
       <h1>It Works!</h1>
